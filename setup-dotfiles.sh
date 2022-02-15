d77c4f33cc053bc6c445370c528b9f84f465de07
Jonathan Prairie <jon.a.prairie@gmail.com>
2022-02-15 16:36:58 -0500

    fix dotfile name clashes when pulling into home directory.

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -5,6 +5,28 @@ HOME=$2
 REPO=$3
 
 cd $HOME
+
+FILES=$HOME/*
+
+for f in $FILES
+do
+    echo "moving $f to $f.old"
+    mv "$f" "$f".old
+done
+
 git init
 git remote add origin $REPO
 git pull origin master
+
+for f in $FILES
+do
+    # apparently bash automatically converts $FILES to have the .old extension?
+    basef=$(basename "$f" .old)
+    echo "checking for $basef"
+    if [ -e $f ]; then
+        echo "moving $f back to $basef"
+        mv $f $basef
+    else
+        "$basef already exists! leaving $f alone."
+    fi
+done
